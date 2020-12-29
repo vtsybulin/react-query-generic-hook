fc93ac1e674a77cb936c31b618e089e7ed0587e0
vtsybulin <vanyatsybulin@gmail.com>
2020-12-29 23:16:03 +0200

    Updates

---
+++ precache-manifest.a8020ceba2a669b29b9b17834f5bb3ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9f2191d3525b2199b2d430bcbce31b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e8883ad328aa12d8f525",
+    "url": "./static/css/2.ef440c72.chunk.css"
+  },
+  {
+    "revision": "edfb129886496ebbd9eb",
+    "url": "./static/css/main.395f7e0b.chunk.css"
+  },
+  {
+    "revision": "e8883ad328aa12d8f525",
+    "url": "./static/js/2.88740b12.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.88740b12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edfb129886496ebbd9eb",
+    "url": "./static/js/main.76027a5c.chunk.js"
+  },
+  {
+    "revision": "46bebdcf9f6c1d2b4101",
+    "url": "./static/js/runtime-main.05934401.js"
+  }
+]);
